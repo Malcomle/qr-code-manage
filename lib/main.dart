90008fa7c6cd53f44cb9b55de6e780f30281c4b5
malcommorel <malcom.morel@gmail.com>
2022-11-15 09:38:21 +0100

    update name and icon of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:qrcode_control/models/is-redirect-model.dart';
-import 'package:qrcode_control/models/redirect-model.dart';
+
+import 'models/is-redirect-model.dart';
+import 'models/redirect-model.dart';
 
 void main() => runApp(MyApp());
 
